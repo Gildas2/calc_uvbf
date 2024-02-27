64ee574fd68fb209c3a4f886eab947cb2c26fd6c
SAMNE Gildas <samnegildas4@gmail.com>
2024-02-27 17:27:43 +0000

    init

--- calculatrice.py
+++ calculatrice.py
@@ -1,53 +1,11 @@
-def addition(x, y):
-    return x + y
+class Calculatrice :
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+        
+    def addition(self):
+        resultat = self.nombre1 +  self.nombre2
+        print(f'{self.nombre1} + {self.nombre2} = {resultat}')
+        return resultat
+        
 
-def soustraction(x, y):
-    return x - y
-
-def multiplication(x, y):
-    return x * y
-
-def division(x, y):
-    if y == 0:
-        return "Erreur : Division par zéro !"
-    else:
-        return x / y
-
-def quitter():
-    print("Merci d'avoir utilisé la calculatrice. À bientôt !")
-
-def main():
-    while True:
-        try:
-            print("\nBienvenue dans la Calculatrice Console")
-            num1 = float(input("Entrez le premier nombre : "))
-            num2 = float(input("Entrez le deuxième nombre : "))
-        except ValueError:
-            print("Erreur : Veuillez entrer des nombres valides.")
-            continue
-
-        print("\nChoisissez l'opération à effectuer :")
-        print("1. Addition")  
-        print("2. Soustraction")
-        print("3. Multiplication")
-        print("4. Division")
-        print("5. Sortie")
-
-        choix = input("Entrez le numéro de l'opération choisie : ")
-
-        if choix == '1':
-            print("Résultat : ", addition(num1, num2))
-        elif choix == '2':
-            print("Résultat : ", soustraction(num1, num2))
-        elif choix == '3':
-            print("Résultat : ", multiplication(num1, num2))    
-        elif choix == '4':
-            print("Résultat : ", division(num1, num2))
-        elif choix == '5':
-            quitter()
-            break
-        else:
-            print("Choix invalide, veuillez réessayer.")
-
-if __name__ == "__main__":
-    main()
