692537b226b388dfd25226ff4b11888b7beadb51
SAMNE Gildas <samnegildas4@gmail.com>
2024-02-28 03:52:50 +0000

    Ajout des fonctions de test pour la multiplication et la soustraction

--- test.py
+++ test.py
@@ -8,6 +8,19 @@ class Testcalculatrice(unittest.TestCase):
         
         calc2 = Calculatrice(1.5, 2.5)
         self.assertTrue(calc2.addition() == 4)
+     
+    def test_mutiplication(self):
+        calc1 = Calculatrice(2, 3)
+        self.assertEqual(calc1.multiplication(), 6) 
         
+        calc2 = Calculatrice(-2, 4)
+        self.assertTrue(calc2.multiplication() == -8)
+        
+    def test_soustraction(self):
+        calc1 = Calculatrice(6, 4)
+        self.assertEqual(calc1.soustraction(), 2)    
+        
+        calc2 = Calculatrice(-4, 5)
+        self.assertTrue(calc2.soustraction() == 1)
 if __name__ == '__main__':
         unittest.main()
